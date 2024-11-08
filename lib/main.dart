77f4d7018192a48514d3081bb0c6fb4e5564add7
KUROO <generikgenis@gmail.com>
2024-11-08 15:13:04 +0700

    penambahan splash cscreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:wisata_mobile5_genis/mystatefullwidget.dart';
+import 'package:wisata_mobile5_genis/screens/splashcreen.dart';
 
-void main()
-{
-  runApp(const Mystatefulwidget());
+void main() {
+  runApp(const TravelApp());
+}
+
+class TravelApp extends StatelessWidget {
+  const TravelApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
+  }
 }
---
+++ lib/utils/const.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const kButtonColor = Color(0xff1B1B1B);
+const kBackgroundColor = Color(0xffF0F3F5);
+const blueTextColor = Color(0XFF2B56CC);
